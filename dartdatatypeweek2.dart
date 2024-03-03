4745cf8b5d6b80b84fb6fae38415c8885570582b
EUTYCUSWANJOHI <wamaieutycus@gmail.com>
2024-03-03 19:43:06 +0300

    EUTYCUSWANJOHI

---
+++ dartdatatypeweek2.dart
@@ -0,0 +1,34 @@
+void main() {
+  // int data type for whole numbers.
+  int number = 40;
+  print(number);
+
+// String data type.it helps store text in the program.
+
+String school = "JOYSTAR ACADEMY";
+print("My first school was $school");
+
+//double data type for numbers with decimal places.
+double averageSize = 30.7;
+print(averageSize);
+
+//Boolean data type. it is usually used to hold either true or false value.
+bool disabilityStatus = false;
+print("any disability: $disabilityStatus");
+
+//list data type which is used to store multiple values without creating multiple variables.
+List<String> friends = ["John", "Albert", "Kelvin"];
+print("My friends include $friends");
+
+//Map data type which represents set of values as key_value pairs.
+Map<String, int> marks = {'kelvin': 30, 'Suleiman': 40, 'Caleb':60};
+print("The Geography class was performed as follows $marks");
+
+//Runes data type is used to represent a unicode code point.
+String runesString = "runes in dart: \u{1F600}";
+print(runesString);
+
+//null
+
+ 
+}
